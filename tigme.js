6b93446a4a962c1379a6d2ded9c9d2b6634aeb7e
hoigag <hoigag@student.1337.ma>
2023-04-16 21:11:01 +0000

    fixed a bg

--- tigme.js
+++ tigme.js
@@ -1,2 +1,6 @@
 console.log("ready");  
-document.querySelector(".btn-primary").disabled = true;
+let button = document.querySelector(".btn-primary");
+let div = document.querySelector(".shop-item");
+button.disabled = true;
+button.remove();
+div.remove();
