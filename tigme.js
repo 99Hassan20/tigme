f88fd3fa8cdae89b2b79c95f9ec0e21452ebd94a
hoigag <hoigag@student.1337.ma>
2023-05-13 15:54:32 +0100

    added somthing new

--- tigme.js
+++ tigme.js
@@ -1,6 +1,10 @@
 console.log("ready");  
-let button = document.querySelector(".btn-primary");
-let div = document.querySelector(".shop-item");
-button.disabled = true;
-button.remove();
+let div = document.querySelectorAll(".shop-item")[1];
 div.remove();
+let buttons = document.querySelectorAll(".btn-primary");
+for (let i = 0; i < buttons.length; i++)
+{
+	if (buttons[i].disabled == false)
+		buttons[i].disabled = true;
+	buttons[i].remove();
+}
